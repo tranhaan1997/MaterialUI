d090638b20195016e7fc3292d1ae29a2f31e00fe
tranhaan <tranhaan1997@gmail.com>
2025-06-26 23:48:26 +0700

    update theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { ThemeProvider } from "@mui/material/styles";
 import Header from "./ui/Header";
-
+import theme from "./ui/Theme";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       Hello World
-    </>
+    </ThemeProvider>
   );
 }
 
